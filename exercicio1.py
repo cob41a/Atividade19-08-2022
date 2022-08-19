650427510a2369640b31dfb0128adccf761f2bba
Lucas <53958097+cob41a@users.noreply.github.com>
2022-08-19 19:33:59 -0300

    Add files via upload

---
+++ exercicio1.py
@@ -0,0 +1,32 @@
+class Mural:
+    def __init__(self):
+        print("Pagina mural")
+        self.Escreva um aviso para a turma = Aviso
+        self.Menu = Menu
+        self.Proxima Atividade = Proxima Atividade
+
+print ("----------")
+
+print ("----------")
+
+class Atividades:
+    def __init__(self):
+        print("Pagina Atividades")
+        self.Ver seus trabalhaos turma = Trabalhos
+        self.Google Agente = Agenda
+        self.Pasta da turma no Drive = Drive
+
+print ("----------")
+
+print ("----------")
+
+class Pessoas:
+    def __init__(self):
+        print("Pagina Pessoas")
+        self.Professores turma = Professores
+        self.Aluno = Aluno
+
+print ("----------")
+
+print ("----------")
+
